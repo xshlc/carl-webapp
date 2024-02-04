29ae31ef0e2ae961a5732a8790c201546dc17be4
sung <heechuw@gmail.com>
2024-02-04 11:21:58 -0500

    minor fixes

--- app/(dashboard)/(routes)/dashboard/page.js
+++ app/(dashboard)/(routes)/dashboard/page.js
@@ -16,18 +16,22 @@ function Dashboard() {
       id: 3,
       name: 'SWENG 411',
     },
+    {
+      id: 4,
+      name: 'CMPSC 465',
+    },
   ]
   return (
     <div>
       <h2 className='text-[23px] font-medium p-5'>My Courses:</h2>
       <div
         className='grid grid-cols-1 sm:grid-cols-2
-        md:grid-cols-3 lg:grid-cols-4 mt-5 gap-5'
+        md:grid-cols-3 lg:grid-cols-4 mt-5 gap-5 m-5'
       >
         {courseList.map((item, index) => (
           <div
-            className='border rounded-lg p-3 ml-5 mr-5
-            cursor-pointer hover:border-purple-300'
+            className='border rounded-lg p-5
+             cursor-pointer hover:border-purple-500'
           >
             <h2>{item.name}</h2>
           </div>
--- app/_components/Header.js
+++ app/_components/Header.js
@@ -46,7 +46,7 @@ function Header() {
               <div className='sm:flex sm:gap-4'>
                 <a
                   className='block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-700'
-                  href='/files'
+                  href='/dashboard'
                 >
                   Get Started
                 </a>
