d80f23781f4ccdb3e0f9ed1371333f72bf252733
sung <heechuw@gmail.com>
2024-02-07 20:16:54 -0500

    upload file to a specific course

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -3,18 +3,18 @@ import React from 'react'
 import { useState } from 'react'
 import UploadForm from './_components/UploadForm'
 import { app } from '@/firebaseConfig'
-'
 import {
   getDownloadURL,
   getStorage,
   uploadBytesResumable,
   ref,
 } from 'firebase/storage'
-// import { getDatabase, push, ref, set } from 'firebase/database'
+import { getDatabase, push, set } from 'firebase/database'
+import { getFirestore, doc, setDoc } from 'firebase/firestore'
 
 function Upload() {
   const [progress, setProgress] = useState()
-  // const database = getDatabase(app)
+  const db = getFirestore(app)
   const storage = getStorage(app)
 
   const uploadFile = file => {
@@ -31,9 +31,22 @@ function Upload() {
       progress == 100 &&
         getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
           console.log('File available at', downloadURL)
-          // saveInfo(file, downloadURL)
+          saveInfo(file, downloadURL)
         })
     })
+  }
+
+  const saveInfo = async (file, fileUrl) => {
+    const docId = Date.now().toString()
+    await setDoc(
+      doc(db, '/Instructor/TestInstructor/Class/SWENG 411/Material', docId),
+      {
+        fileName: file.name,
+        fileSize: file.size,
+        fileType: file.type,
+        fileUrl: fileUrl,
+      },
+    )
   }
   return (
     <div className='p-5 px-8 md:px-28'>
