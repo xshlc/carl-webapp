28e661d6545684e18510dd5ccfb8f79267e9eba4
sung <heechuw@gmail.com>
2024-02-09 12:30:29 -0500

    firebase env deployment fix

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,20 +1,18 @@
 // Import the functions you need from the SDKs you need
-require('dotenv').config()
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: process.env.APP_API_KEY,
+  authDomain: process.env.APP_AUTH_DOMAIN,
+  databaseURL: process.env.APP_DATABASE_URL,
+  projectId: process.env.APP_PROJECT_ID,
+  storageBucket: process.env.APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.APP_MESSAGING_SENDER_ID,
+  appId: process.env.APP_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
